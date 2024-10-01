42d565a410be39a61ac9c9e783a21d48980de5d8
kiraum <tfgoncalves@xpto.it>
2024-10-01 11:49:36 +0200

    chore(askJira): new release v0.0.3

--- Formula/askjira.rb
+++ Formula/askjira.rb
@@ -5,7 +5,7 @@ class Askjira < Formula
   homepage "https://github.com/kiraum/askJira"
   url "https://github.com/kiraum/askJira/raw/main/releases/v#{version}/askJira-v#{version}-macos-arm64.tar.gz",
       using: CurlDownloadStrategy
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  sha256 "8057e672bdfe3514a4f066aa9f8b0b23c09067720a56c1e7c235fe2e0b5377d2"
   version version
   license "MIT"
 
